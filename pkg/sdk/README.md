8677bcec61effea1e0fca2b7bc684b298fb425ee
Roland Szarka-Kovács <Roland.Szarka-Kovacs@meshx.co>
2022-02-26 19:06:09 +0100

    feat(sdk): export process & realm

---
+++ pkg/sdk/README.md
@@ -0,0 +1,10 @@
+# `@meshx-org/fiber-sdk`
+
+> TODO: description
+
+## Usage
+
+```ts
+import { System } from '@meshx-org/fiber-sdk';
+System.init(...)
+```
--- pkg/sdk/src/index.ts
+++ pkg/sdk/src/index.ts
@@ -1,3 +1,5 @@
 export * from './system'
 export * from './handles/channel'
 export * from './handles/handle'
+export * from './handles/process'
+export * from './handles/realm'
