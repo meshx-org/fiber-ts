d369fdb6e86e3803e4d8de1fc6f581a3af8fcd38
Roland Szarka-Kovács <Roland.Szarka-Kovacs@meshx.co>
2022-02-27 20:05:49 +0100

    feat: added kernel options

--- pkg/kernel/src/kernel.ts
+++ pkg/kernel/src/kernel.ts
@@ -1,6 +1,5 @@
-/* eslint-disable no-with */
-/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-bitwise */
+
 import {
   Handle as RawHandle,
   Channel as RawChannel,
@@ -30,28 +29,38 @@ const ROOT_REALM_KOID: Koid = 1
 const ROOT_REALM_HANDLE: RawHandle = 1
 
 type Koid = number
-// type ProcessHandle = [RawHandle, RawProcess]
-// type BIF = (...args: unknown[]) => unknown
 
-interface KernelHandle {
+interface IKernelHandle {
   koid: number
   rights: HandleRights
 }
 
+type OnProcessCreateFn = (process: ProcessObject) => void
+type OnProcessKillFn = (process: ProcessObject) => void
+
+export interface IKernelOptions {
+  onProcessCreate: OnProcessCreateFn
+  onProcessKill: OnProcessKillFn
+}
+
 export default class Kernel implements ISyscalls {
-  readonly #klog: ILogger
+  private readonly klog: ILogger
+
+  private readonly onProccessCreate: OnProcessCreateFn
+  private readonly onProccessKill: OnProcessKillFn
 
   private kobjects: Record<Koid, KernelObject> = {}
-  private handles: Record<RawHandle, KernelHandle> = {}
+  private handles: Record<RawHandle, IKernelHandle> = {}
 
   // private processHandles: Map<ProcessHandle, KernelHandle> = new Map()
-  // private bifs: Map<string, BIF> = new Map()
 
   private koidGen = new IdGen()
   private handleGen = new IdGen()
 
-  constructor() {
-    this.#klog = createKernelLogger()
+  constructor(options: IKernelOptions) {
+    this.klog = createKernelLogger()
+    this.onProccessCreate = options.onProcessCreate
+    this.onProccessKill = options.onProcessKill
 
     // create root realm object
     this.kobjects[ROOT_REALM_KOID] = new RealmObject()
@@ -62,46 +71,47 @@ export default class Kernel implements ISyscalls {
   }
 
   public get rootRealm(): RawHandle {
-    this.#klog.log('access root realm')
+    this.klog.log('access root realm')
     return ROOT_REALM_HANDLE
   }
 
   // #region handle operations
   public handleDuplicate(handle: RawHandle): HandleResult {
-    this.#klog.log('handleDuplicate', handle)
+    this.klog.log('handleDuplicate', handle)
     return { status: Status.OK, handle: INVALID_HANDLE }
   }
 
   public handleReplace(handle: RawHandle, replacement: RawHandle): HandleResult {
-    this.#klog.log('handleReplace', handle, replacement)
+    this.klog.log('handleReplace', handle, replacement)
     return { status: Status.OK, handle: INVALID_HANDLE }
   }
 
   public handleClose(handle: RawHandle): Result {
-    this.#klog.log('handleClose', handle)
+    this.klog.log('handleClose', handle)
     return { status: Status.OK }
   }
   // #endregion
 
   // #region realm operations
   public realmCreate(parent: RawHandle): Result {
+    this.klog.log('realmCreate', parent)
     return { status: Status.OK }
   }
 
   public realmListen(realm: RawHandle, multiaddr: string, channel: RawChannel): Result {
-    this.#klog.log('realmListen')
+    this.klog.log('realmListen', realm, multiaddr, channel)
     return { status: Status.OK }
   }
 
   public realmDial(realm: RawHandle, multiaddr: string, channel: RawChannel): Result {
-    this.#klog.log('realmDial')
+    this.klog.log('realmDial')
     return { status: Status.OK }
   }
   // #endregion
 
   // #region process operations
   public processCreate(parentRealm: RawRealm, name: string, programVmo: RawHandle): HandleResult {
-    this.#klog.log('processCreate', parentRealm, programVmo)
+    this.klog.log('processCreate', parentRealm, programVmo)
 
     let handleId: number
     let processKoid: number
@@ -125,34 +135,34 @@ export default class Kernel implements ISyscalls {
   }
 
   public processStart(process: RawProcess, bootChannel: RawChannel) {
-    this.#klog.log('processStart', process)
+    this.klog.log('processStart', process)
     return { status: Status.OK }
   }
   // #endregion
 
   // #region channel operations
   public channelCreate(): HandlePairResult {
-    this.#klog.log('channelCreate')
+    this.klog.log('channelCreate')
     return { status: Status.OK, first: INVALID_HANDLE, second: INVALID_HANDLE }
   }
 
   public channelWrite(channel: RawHandle, data: Uint8Array, handles: RawHandle[]): WriteResult {
-    this.#klog.log('channelWrite', channel, data, handles)
+    this.klog.log('channelWrite', channel, data, handles)
     return { status: Status.OK, numBytes: 0 }
   }
 
   public channelWriteEtc(channel: RawHandle, data: Uint8Array, handleDispositions: HandleDisposition[]): WriteResult {
-    this.#klog.log('channelWriteEtc', channel, data, handleDispositions)
+    this.klog.log('channelWriteEtc', channel, data, handleDispositions)
     return { status: Status.OK, numBytes: 0 }
   }
 
   public channelRead(channel: RawHandle): ReadResult {
-    this.#klog.log('channelRead', channel)
+    this.klog.log('channelRead', channel)
     return { status: Status.OK, numBytes: 0, bytes: new Uint8Array(0), handles: [] }
   }
 
   public channelReadEtc(channel: RawHandle): ReadEtcResult {
-    this.#klog.log('channelReadEtc', channel)
+    this.klog.log('channelReadEtc', channel)
     return { status: Status.OK, numBytes: 0, bytes: new Uint8Array(0), handleInfos: [] }
   }
   // #endregion
